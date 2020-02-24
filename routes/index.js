828589eaf2709f45470e9e84032afe3c6a50bba6
Martin Chiteri <martin.chiteri@gmail.com>
2020-02-24 03:51:39 +0000

    Allow for users to create and view profiles

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express  = require("express"), 
 	  passport = require("passport"),
-	  User = require("../models/user");
+	  User = require("../models/user"), 
+	  Campground = require("../models/campground");
 
 var router = express.Router();
 
@@ -11,7 +12,12 @@ router.get("/register", (req, res) => {
 
 // Handle sign up logic
 router.post("/register", (req, res) => {
-	var newUser = new User({username: req.body.username});
+	var newUser = new User({username: req.body.username, 
+							firstName: req.body.firstName, 
+							lastName: req.body.lastName, 
+							avatar: req.body.avatar, 
+						   email: req.body.email 
+						   });
 	
 	if (req.body.adminCode === "secretcode123") {
 		newUser.isAdmin = true;
@@ -48,6 +54,23 @@ router.get("/logout", (req, res) => {
 	req.logout();
 	req.flash("success", "Logged you out!");
 	res.redirect("/campgrounds");
+});
+
+// USER PROFILE
+router.get("/users/:id", (req, res) => {
+	User.findById(req.params.id, (err, foundUser) => {
+		if (err) {
+			req.flash("error", "Something went wrong ...");
+			res.redirect("/");
+		} 
+		Campground.find().where("author.id").equals(foundUser.id).exec((err, campgrounds) => {
+			if (err) {
+				req.flash("error", "Something went wrong ...");
+				res.redirect("/");
+			} 
+			res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+		});		
+	});
 });
 
 module.exports = router;
